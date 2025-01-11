0c0999d755b553c0b80bc96c1b0cee94094dab04
HAJARA  B <hajarabasheer26@gmail.com>
2025-01-11 12:02:48 +0530

    Update 1.8_Set_using_Bit_String.c

--- 1.8_Set_using_Bit_String.c
+++ 1.8_Set_using_Bit_String.c
@@ -1,55 +1,194 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+
+#define MAX_SIZE 50
+
+int uni[MAX_SIZE],set1[MAX_SIZE],set2[MAX_SIZE];
+int bitstringset1[MAX_SIZE],bitstringset2[MAX_SIZE],unionarray[MAX_SIZE],intersectionarray[MAX_SIZE];
+int num,num1,num2,i,temp,exists,j=0,choice;
+
+int isElementInSet(int element,int set[],int setSize);
+void union_bitsring();
+void intersection_bitstring();
+
+void main()
+{
+printf("\nenter the size of the universal set: \n");
+scanf("%d",&num);
+
+while(num<=0 || num>MAX_SIZE)
+{
+    printf("\ninvalid size...\n");
+    scanf("%d",&num);
+}
+
+printf("\nenter the elements of the universal set: \n");
+for(j=0;j<num;)
+{
+    exists=0;
+    printf("\nenter element %d: \n",j+1);
+    scanf("%d",&temp);
 
 
-void add_element(int *bitset, int element) {
-    *bitset |= (1 << element);  
+if (isElementInSet(temp,uni,j))
+{
+    printf("\nelement already exist...\n");
+    
+}
+else{
+    uni[j]=temp;
+    j++;
+}
+}
+
+printf("\n------universal set------\n");
+for(i=0;i<num;i++)
+{
+    printf("%d \n" ,uni[i]);
+}
+
+j=0;
+printf("\nenter the size of set1: \n");
+scanf("%d",&num1);
+
+while(num1<=0 || num1>50 )
+{
+    printf("\ninvalid size...\n");
+    scanf("%d",&num1);
+}
+
+printf("\nenter the elements of set1: \n");
+for(j=0;j<num1;)
+{
+    exists=0;
+    printf("\nenter element %d: \n",j+1);
+    scanf("%d",&temp);
+
+
+if (isElementInSet(temp,set1,j))
+{
+    printf("\nelement already exists...\n");
+    
+}
+else
+{
+    set1[j]=temp;
+    j++;
+}
+}
+
+printf("\n------set 1------\n");
+for(i=0;i<num1;i++)
+{
+    printf("%d \n",set1[i]);
+}
+
+j=0;
+printf("\nenter the size of set2: \n");
+scanf("%d",&num2);
+
+while(num2<=0 || num2>50 )
+{
+    printf("\ninvalid size...\n");
+    scanf("%d",&num2);
+}
+
+printf("\nenter the elements of set2: \n");
+for(j=0;j<num2;)
+{
+    exists=0;
+    printf("\nenter element %d: \n",j+1);
+    scanf("%d",&temp);
+
+
+if (isElementInSet(temp,set2,j))
+{
+    printf("\nelement already exists...\n");
+
+}
+else
+{
+    set2[j]=temp;
+    j++;
+}
+}
+printf("\n------set 2------\n");
+for(i=0;i<num2;i++)
+{
+    printf("%d \n",set2[i]);
+}
+
+for(i=0;i<num;i++)
+{
+    bitstringset1[i]=isElementInSet(uni[i],set1,num1);
+    bitstringset2[i]=isElementInSet(uni[i],set2,num2);
+}
+
+printf("\n------bitstring 1------\n");
+for(i=0;i<num;i++)
+{
+    printf("%d",bitstringset1[i]);
 }
 
 
-void remove_element(int *bitset, int element) {
-    *bitset &= ~(1 << element);  
+printf("\n------bitstring 2------\n");
+for(i=0;i<num;i++)
+{
+    printf("%d",bitstringset2[i]);
 }
 
+do
+{
+    printf("\nenter your choice: \n 1.union\n 2.intersection\n 3.exit\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
 
-int is_member(int bitset, int element) {
-    return (bitset & (1 << element)) != 0; 
+     case 1:
+        union_bitsring();
+        break;
+
+     case 2:
+        intersection_bitstring();
+        break;
+
+     case 3:
+        printf("Exiting...\n");
+        break;
+
+     default:
+        printf("invalid choice\n");
+    }
+}while (choice!=3);
 }
 
-
-void display_set(int bitset, int size) {
-    printf("Set contains: { ");
-    for (int i = 0; i < size; i++) {
-        if (is_member(bitset, i)) {
-            printf("%d ", i);
+int isElementInSet(int element,int set[],int setSize)
+{
+    for(int i=0;i<setSize;i++)
+    {
+        if(set[i]==element)
+        {
+            return 1;
         }
     }
-    printf("}\n");
+    return 0;
 }
 
-int main() {
-    int bitset = 0;  
-    int size = 10;   
+void union_bitsring()
+{
+    printf("\nunion of bitstrings is: \n");
+    for(i=0;i<num;i++)
+    {
+        unionarray[i]=bitstringset1[i] | bitstringset2[i];
+        printf("%d",unionarray[i]);
+    }
+}
 
-
-    add_element(&bitset, 3);   
-    add_element(&bitset, 5);   
-    add_element(&bitset, 7);   
-    add_element(&bitset, 9);   
-
-   
-    display_set(bitset, size);
-
-
-    printf("Is 3 in the set? %s\n", is_member(bitset, 3) ? "Yes" : "No");
-    printf("Is 4 in the set? %s\n", is_member(bitset, 4) ? "Yes" : "No");
-
-    
-    remove_element(&bitset, 5);  
-    remove_element(&bitset, 9);  
-
-  
-    display_set(bitset, size);
-
-    return 0;
+void intersection_bitstring()
+{
+    printf("\nintersection of bitstrings is: \n");
+    for(i=0;i<num;i++)
+    {
+        intersectionarray[i]=bitstringset1[i] & bitstringset2[i];
+        printf("%d",intersectionarray[i]);
+    }
 }
