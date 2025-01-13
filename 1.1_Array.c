13d18f28b4519868e8e0ec7eae4c97be46df7656
HAJARA  B <hajarabasheer26@gmail.com>
2025-01-13 21:10:12 +0530

    Update 1.1_Array.c

--- 1.1_Array.c
+++ 1.1_Array.c
@@ -1,106 +1,132 @@
 #include<stdio.h>
 #include<stdlib.h>
-void insert(int a[],int size);
-void deleteElement(int a[],int size);
-void search(int a[],int size);
-void sort(int a[],int size);
-void display(int a[],int size);
+
+int a[50],size=0;
+
+void insertion(int ins,int pos)
+{
+	if(pos<1 || pos>size+1)
+	{
+		printf("invalid position\n");
+	}
+	else
+	{
+		for(int i=size;i>=pos;i--)
+		{
+			a[i]=a[i-1];
+		}
+		a[pos-1]=ins;
+		size++;
+	}
+}
+
+void deletion(int pos)
+{
+	if(pos<1 || pos>size)
+	{
+		printf("invalid position\n");
+	}
+	else
+	{
+		for(int i=pos-1;i<size-1;i++)
+		{
+			a[i]=a[i+1];
+		}
+		size--;
+	}
+}
+
+void sort()
+{
+	int temp;
+	for(int i=0;i<size-1;i++)
+	{
+		for(int j=0;j<size-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+
+	}
+}
+
+int search(int value)
+{
+	int pos=0;
+	for(int i=0;i<size;i++)
+	{
+		if(a[i]==value)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void display()
+{
+	printf("------ARRAY ELEMENTS------\n");
+	for(int i=0;i<size;i++)
+	{
+		printf("%d \n",a[i]);
+	}
+}
+
 int main()
 {
-int i,a[50],size,ch;
-printf("enter the size of the array: ");
-scanf("%d",&size);
-printf("enter the elements of the array: ");
-for(i=0;i<size;i++);
-{
-scanf("%d",&a[i]);
-}
-while(1)
-{
-printf("enter your choice: 1.insert\n2.deleteElement\n3.search\n4.sort\n5.display\n6exit\n");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1: insert(a,size);
-        break;
-case 2: deleteElement(a,size);
-        break;
-case 3: search(a,size);
-        break;3
-case 4: sort(a,size);
-        break;
-case 5: display(a,size);
-        break;
-case 6: return 0;
-        break;
-default:printf("invalid choice");
-        break;   
-}
-}
-}
-void insert(int a[],int size)
-{
-int i,ins,pos;
-printf("enter the element to be inserted: ");
-scanf("%d",&ins);
-printf("enter the position of the element: ");
-scanf("%d",&pos);
-for(i=size-1;i>=pos-1;i--)
-{
-a[i+1]=a[i];
-}
-a[pos-1]=ins;
-size++;
-}
-void deleteElement(int a[],int size)
-{
-int i,del,pos;
-printf("enter the element to be deleted: ");
-scanf("%d",&del);
-for(i=pos-1;i<size-1;i++)
-{
-a[i]=a[i+1];
-}
-}
-void search(int a[],int size)
-{
-int i,ele;
-printf("enter the element to be searched: ");
-scanf("%d",&ele);
-for(i=0;i<size;i++)
-{
-if(a[i]==ele)
-{
-printf("element found at %d",i);
-}
-if(a[i]==size)
-{
-printf("element is not found");
-}
-}
-}
-void sort(int a[],int size)
-{
-int i,j,temp;
-for(i=0;i<size-1;i++)
-{
-for(j=0;j<size-1;j++)
-{
-if(a[j]>a[j+1])
-{
-temp=a[j];
-a[j]=a[j+1];
-a[j+1]=temp;
-}
-}
-}
-}
-void display(int a[],int size)
-{
-int i;
-printf("the array elements are: ");
-for(i=0;i<size;i++)
-{
-printf("%d\n",a[i]);
-}
+	int ins,pos,value,choice;
+	while(1)
+	{
+		printf("enter your choice\n 1.insert\n 2.delete\n 3.sort\n 4.search\n 5.dispaly\n 6.exit\n");
+		scanf("%d",&choice);
+
+		switch(choice)
+		{
+			case 1:
+			printf("enter the element to insert: ");
+			scanf("%d",&ins);
+			printf("enter the position to insert: ");
+			scanf("%d",&pos);
+			insertion(ins,pos);
+			break;
+
+			case 2:
+			printf("enter the position to delete: ");
+			scanf("%d",&pos);
+			deletion(pos);
+			break;
+
+			case 3:
+			sort();
+			break;
+
+			case 4:
+			printf("enter the element to search: ");
+			scanf("%d",&value);
+			pos=search(value);
+			if(pos==-1)
+			{
+				printf("element not found\n");
+			}
+			else
+			{
+				printf("element found at %d th position\n",pos+1);
+			}
+			break;
+
+			case 5:
+			display();
+			break;
+
+			case 6:
+			exit(0);
+
+			default:
+			printf("invalid choice\n");		
+		}
+	}
 }
